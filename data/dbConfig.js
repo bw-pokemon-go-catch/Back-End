06a88f9c3fbc1f6c49c2bd5745fe4c0df3192007
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-07-27 21:27:59 -0500

    saturday night commit

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,6 @@
 const knex = require("knex");
-const knexConfig = require("../knexfile.js");
+const config = require("../knexfile.js");
 
-module.exports = knex(knexConfig.development);
+const environment = process.env.DB_ENV || 'development';
+
+module.exports = knex(config[environment]);
