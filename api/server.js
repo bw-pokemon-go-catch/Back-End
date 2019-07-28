73bb2262a48d9194f04922940c1b4e78da481a5d
Eric Fugleberg <ericfugleberg@Erics-MBP.hsd1.co.comcast.net>
2019-07-28 15:33:40 -0600

    added server get

--- api/server.js
+++ api/server.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const cors = require('cors')
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 const server = express();
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
-const authRouter = require('../auth/authRouter.js');
-const pokemonRouter = require('../pokemon/pokemonRouter.js');
-const usersRouter = require('../users/usersRouter.js');
+const authRouter = require("../auth/authRouter.js");
+const pokemonRouter = require("../pokemon/pokemonRouter.js");
+const usersRouter = require("../users/usersRouter.js");
 
+server.use("/auth", authRouter);
+server.use("/pokemon", pokemonRouter);
+server.use("/users", usersRouter);
 
-server.use('/auth', authRouter);
-server.use('/pokemon', pokemonRouter);
-server.use('/users', usersRouter);
-
+server.get("/", (req, res) => {
+  res.json({ message: "API is up!" });
+});
 
 module.exports = server;
