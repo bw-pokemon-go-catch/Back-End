8d18093fe72ec55b4147895a4c94adae2f1ff329
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-07-27 19:23:47 -0500

    endpoints working - pg installed

--- data/migrations/20190726231313_pokemon.js
+++ data/migrations/20190726231313_pokemon.js
@@ -1,21 +1,21 @@
 exports.up = function(knex) {
-    return knex.schema.exports("pokemon", tbl => {
-      tbl.increments();
-  
-      tbl
-        .string("Pokemon No.", 256)
-        .notNullable()
-        .unique();
-  
-      tbl.string("Name", 300).notNullable();
-      tbl.string("Type 1", 300).notNullable();
-      tbl.string("Type 2", 300)
-      tbl.string("Max CP", 300).notNullable();
-      tbl.string("Max HP", 300).notNullable();
-      tbl.string("Image URL", 500).notNullable();
-    });
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTableIfExists("pokemon");
-  };
+  return knex.schema.createTable("pokemon", tbl => {
+    tbl.increments();
+
+    tbl
+      .string("Pokemon No", 256)
+      .notNullable()
+      .unique();
+
+    tbl.string("Name", 300).notNullable();
+    tbl.string("Type 1", 300).notNullable();
+    tbl.string("Type 2", 300);
+    tbl.integer("Max CP", 300).notNullable();
+    tbl.integer("Max HP", 300).notNullable();
+    tbl.string("Image URL", 500).notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("pokemon");
+};
